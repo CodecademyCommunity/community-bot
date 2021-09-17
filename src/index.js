b20b2908af05603c7320916ead7c388e71e71f9e
aedwardg <aedwardg1@gmail.com>
2021-09-16 22:26:50 -0700

    Separate events into modules

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const { Client, Collection, Intents } = require('discord.js');
 require('dotenv').config();
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const commandsDir = `${__dirname}/commands`;
+const eventsDir = `${__dirname}/events`;
 
 client.commands = new Collection();
-const commandsDir = `${__dirname}/commands`;
+
 const commandFiles = fs
   .readdirSync(commandsDir)
   .filter((file) => file.endsWith('.js'));
@@ -15,26 +17,17 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
-client.once('ready', () => {
-  console.log('Ready!');
-});
+const eventFiles = fs
+  .readdirSync(eventsDir)
+  .filter((file) => file.endsWith('.js'));
 
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: 'There was an error while executing this command!',
-      ephemeral: true,
-    });
+for (const file of eventFiles) {
+  const event = require(`${eventsDir}/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-});
+}
 
 client.login(process.env.BOT_TOKEN);
