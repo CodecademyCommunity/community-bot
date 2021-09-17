e263781ff051fa0cbad1e9f86861cdc4603e437e
aedwardg <aedwardg1@gmail.com>
2021-09-17 13:40:27 -0700

    refactor /format command

--- src/commands/format.js
+++ src/commands/format.js
@@ -11,31 +11,35 @@ module.exports = {
         .setDescription('Programming language')
         .setRequired(false)
     ),
-  execute(interaction) {
-    let language = interaction.options.getString('language', false);
-    language = language || '';
-
-    interaction.reply('Please enter a code block.').then(() => {
-      const filter = (m) => interaction.user.id == m.author.id;
-
-      interaction.channel
-        .awaitMessages({
-          filter,
-          time: 60000,
-          max: 1,
-          errors: ['time'],
-        })
-        .then((messages) => {
-          const msg = messages.first();
-          const code = msg.content;
-          interaction.channel.messages.delete(msg.id);
-          const formatted = codeBlock(language, code);
-          console.log(formatted);
-          interaction.followUp(formatted);
-        })
-        .catch(() => {
-          interaction.followUp('You did not enter a code block!');
-        });
+  async execute(interaction) {
+    await interaction.reply({
+      content: 'Please enter a code block.',
+      fetchReply: true,
     });
+
+    try {
+      await formatCode(interaction);
+    } catch (error) {
+      interaction.followUp('You did not enter a code block!');
+    }
   },
 };
+
+async function formatCode(interaction) {
+  const language = interaction.options.getString('language', false) || '';
+  const filter = (m) => interaction.user.id == m.author.id;
+
+  const messages = await interaction.channel.awaitMessages({
+    filter,
+    time: 60000,
+    max: 1,
+    errors: ['time'],
+  });
+
+  const msg = messages.first();
+  const code = msg.content;
+  interaction.channel.messages.delete(msg.id);
+
+  const formatted = codeBlock(language, code);
+  interaction.editReply(formatted);
+}
