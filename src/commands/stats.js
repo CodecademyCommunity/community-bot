b16bbb5c2249d64ff00a8f814efc455895f13a49
Victoria-DR <Victoria-DR@users.noreply.github.com>
2021-10-03 02:19:33 +0000

    Apply Prettier/ESLint fixes with CI

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -3,16 +3,18 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName('stats')
-  .setDescription('Display basic server stats.'),
-  
+    .setName('stats')
+    .setDescription('Display basic server stats.'),
+
   async execute(interaction) {
     const fetchedMembers = await interaction.guild.members.fetch();
     const totalOnline = fetchedMembers.filter(
-      (member) => (member.presence != null && member.presence.status !== 'offline')
-      ).size;
+      (member) =>
+        member.presence != null && member.presence.status !== 'offline'
+    ).size;
     const totalOffline = fetchedMembers.filter(
-      (member) => (member.presence == null || member.presence.status === 'offline')
+      (member) =>
+        member.presence == null || member.presence.status === 'offline'
     ).size;
 
     const Embed = new MessageEmbed();
@@ -20,7 +22,7 @@ module.exports = {
     Embed.addField('Online Members', `${totalOnline}`);
     Embed.addField('Offline Members', `${totalOffline}`);
     Embed.addField('Total Members', `${interaction.guild.memberCount}`);
-    
+
     interaction.reply({ embeds: [Embed] });
   },
 };
